4d62867c934191cbcb31e8a7c1fc39ab7cc844a7
FrontVico <vinicius.amorim31@etec.sp.gov.br>
2024-09-11 14:13:41 -0300

    Atualização 3.0

--- mostruario/mostruario.js
+++ mostruario/mostruario.js
@@ -3,7 +3,7 @@
     const produtos = [
         {
             nome: "Camiseta Street-Joker",
-            preco: "R$ 79,99",
+            valor: "R$ 79,99",
         }
     ];
 
@@ -11,6 +11,11 @@
     function displayProductName() {
         const productNameElement = document.getElementById('productName');
         productNameElement.textContent = produtos[0].nome; // Pega o nome do primeiro produto
+    }
+
+    function displayProductName() {
+        const productNameElement = document.getElementById('valor');
+        productNameElement.textContent = produtos[0].valor; // Pega o nome do primeiro produto
     }
 
     // Chama a função quando a página carregar
